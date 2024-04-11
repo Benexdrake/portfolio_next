69ff2d89ad301b0accf7739f31c59f49895e1e76
BeneX Drake <benex@hotmail.de>
2024-04-11 22:56:01 +0200

    update

--- pages/api/crunchyroll/animes.ts
+++ pages/api/crunchyroll/animes.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import animesDB from './Animes.json'
+import animesDB from '@/public/Animes.json'
 import { Anime } from '@/types/anime';
  
 export default async function handler(
