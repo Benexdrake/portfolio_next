36d39264b9d33c40ac9e4014c2c869a9208fdfb7
BeneX Drake <benex@hotmail.de>
2024-04-11 20:05:01 +0200

    adding project Card

---
+++ components/project/projectCard.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import style from '@/style_modules/projectCard.module.css'
+
+export default function ProjectCard(props: any) {
+  const image = props.image;
+  const description = props.description;
+  const route = props.route;
+
+  return (
+    <Link href={route} style={{ textDecoration: 'none' }}>
+
+<div className={style.card}>
+    <div className={style.content}>
+      <p className={style.description}>{description}</p>
+    </div>
+    <img src={`/assets/projects/${image}`} alt="" />
+  </div>
+    </Link>
+  );
+
+}
