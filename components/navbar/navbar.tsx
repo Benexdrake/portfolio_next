a829eab71af0d255f0c4c03ef721e470ee380538
BeneX Drake <benex@hotmail.de>
2024-04-11 20:04:50 +0200

    deleted session and impressum

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,11 +1,8 @@
 import Link from "next/link";
-import {signIn, signOut, useSession} from 'next-auth/react'
-
 import style from '@/style_modules/navbar.module.css'
 
 export default function Navbar()
 {
-    const {data:session} = useSession();
     return (
         <nav className={style.navbar_body}>
             <div className={style.navbar_links}>
@@ -13,23 +10,8 @@ export default function Navbar()
                     <li><Link href="/">About</Link></li>
                     <li><Link href="/resume">Resume</Link></li>
                     <li><Link href="projects">Projects</Link></li>
-                    <li><Link href="impressum">Impressum</Link></li>
-                    {session && (
-                        <li><Link href="admin">Admin</Link></li>
-                    )}
                 </ul>
             </div>
-            {/* { !session && 
-          <>
-            <button className={style.button} onClick={() => signIn('discord')}>Sign In</button>
-          </>
-        }
-
-        { session && 
-          <>
-            <button  className={style.button} onClick={() => signOut()}>Sign Out</button>
-          </> 
-        } */}
     </nav>
     )
 }
