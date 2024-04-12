140d8dd504cdb51136652677c3ea069acf4673ca
BeneX Drake <benex@hotmail.de>
2024-04-12 08:27:44 +0200

    searching with input works

--- pages/projects/crunchyroll/index.tsx
+++ pages/projects/crunchyroll/index.tsx
@@ -2,13 +2,29 @@ import AnimeCard from "@/components/anime/card";
 import styles from '@/style_modules/crunchyroll/index.module.css'
 import getAnimes from "@/services/crunchyroll/get_animes";
 import getAnimesByName from "@/services/crunchyroll/get_animes_by_name";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Crunchyroll(props: any) {
     let animes = props.animes;
+
+    const router = useRouter();
+
+    let [search, useSearch] = useState('')
+    let [page, usePage] = useState(1)
+
+    let searchKeyHandler = (e:any) => 
+    {
+        if(e.key === 'Enter')
+        {
+            router.push(`/projects/crunchyroll?page=1&name=${search}`)
+        }
+    }
+
     return (
         <>
             <h1 className={styles.title}>Crunchyroll</h1>
-            <input className={styles.input} type="text" />
+            <input className={styles.input} type="text" onKeyDown={searchKeyHandler} onChange={event => useSearch(event.target.value)}/>
             <div className={styles.cards}>
                 {animes.map((x: any) => { return <AnimeCard anime={x} /> })}
             </div>
